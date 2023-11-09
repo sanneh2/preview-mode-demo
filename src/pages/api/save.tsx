e5d6bbdec2b4cf653e1af6d10e05504e98d8ec25
sanneh2 <sppower@gmail.com>
2023-11-09 19:46:44 +0100

    Initial. Edit HTML Static Next.js Webpage with DB

--- src/pages/api/save.tsx
+++ src/pages/api/save.tsx
@@ -1,14 +1,14 @@
-import S3 from 'aws-sdk/clients/s3';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { generate as generateId } from 'shortid';
+import S3 from "aws-sdk/clients/s3";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { generate as generateId } from "shortid";
+import clientPromise from "../../lib/db";
 
-// Initialize S3 instance in module scope for re-use across requests.
-const s3 = new S3({
-  credentials: {
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-  },
-});
+// Check for mongodb connection string in environment variables
+if (!process.env.MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Generate a friendly ID for this save request:
@@ -22,16 +22,25 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   // We're not worried about ID collisions, but a real application probably
   // should be!
   try {
-    await s3
+    /*  await s3
       .upload({
         Bucket: process.env.AWS_S3_BUCKET,
         Key: `${snapshotId}.json`,
         Body: JSON.stringify(contents),
       })
-      .promise();
+      .promise(); */
+
+    const client = await clientPromise;
+    const db = await client.db("radiant-light");
+    const collection = await db.collection("contents");
+    const result = await collection.insertOne({
+      id: snapshotId,
+      contents,
+      timestamp: new Date().toISOString(),
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
     return res.end();
   }
 
@@ -43,6 +52,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 export const config = {
   api: {
-    bodyParser: { sizeLimit: '256kb' },
+    bodyParser: { sizeLimit: "256kb" },
   },
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,15 @@
-import S3 from 'aws-sdk/clients/s3';
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import { useCallback, useRef, useState } from 'react';
-import Edit from '../components/edit';
-import { ErrorDialog } from '../components/error';
-import { ShareLinkDialog } from '../components/home/ShareLinkDialog';
-import Malleable, { FieldEdit } from '../components/malleable';
-import Snapshot from '../components/snapshot';
-import { useScrollReset } from '../hooks/use-scroll-reset';
-import layoutStyles from '../styles/layout.module.css';
-
-// Next.js automatically eliminates code used for `getStaticProps`!
-// This code (and the `aws-sdk` import) will be absent from the final client-
-// side JavaScript bundle(s).
-const s3 = new S3({
-  credentials: {
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-  },
-});
+import S3 from "aws-sdk/clients/s3";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import Edit from "../components/edit";
+import { ErrorDialog } from "../components/error";
+import { ShareLinkDialog } from "../components/home/ShareLinkDialog";
+import Malleable, { FieldEdit } from "../components/malleable";
+import Snapshot from "../components/snapshot";
+import { useScrollReset } from "../hooks/use-scroll-reset";
+import layoutStyles from "../styles/layout.module.css";
+import clientPromise from "../lib/db";
 
 export const getStaticProps: GetStaticProps = async ({
   // `preview` is a Boolean, specifying whether or not the application is in
@@ -34,16 +25,18 @@ export const getStaticProps: GetStaticProps = async ({
     try {
       // In preview mode, we want to access the stored data from AWS S3.
       // Imagine using this to fetch draft CMS state, etc.
-      const object = await s3
-        .getObject({
-          Bucket: process.env.AWS_S3_BUCKET,
-          Key: `${snapshotId}.json`,
-        })
-        .promise();
-
-      const contents = JSON.parse(object.Body.toString());
+      const client = await clientPromise;
+      const db = await client.db("radiant-light");
+      const collection = await db.collection("contents");
+      const contents = await collection.findOne(
+        { id: snapshotId },
+        { projection: { _id: 0 } }
+      );
+      console.log(contents); // Return the array
+      const contentsToReturn = contents.contents;
+      /*       const contents = JSON.parse(object.Body.toString()); */
       return {
-        props: { isPreview: true, snapshotId, contents },
+        props: { isPreview: true, snapshotId, contents: contentsToReturn },
       };
     } catch (e) {
       return {
@@ -54,18 +47,38 @@ export const getStaticProps: GetStaticProps = async ({
             // 403 implies 404 in this case, as our IAM user has access to all
             // objects, but the bucket itself is private.
             e.statusCode === 403
-              ? 'The requested preview edit does not exist!'
-              : 'An error has occurred while connecting to S3. Please refresh the page to try again.',
+              ? "The requested preview edit does not exist!"
+              : "An error has occurred while connecting to S3. Please refresh the page to try again.",
         },
       };
     }
   }
-  return { props: { isPreview: false } };
+  // Grab the latest data for the page from Mongodb
+  const client = await clientPromise;
+  const db = await client.db("radiant-light");
+  const collection = await db.collection("contents");
+  const latestPost = await collection
+    .find({}, { projection: { _id: 0 } }) // Find all documents without _id
+    .sort({ timestamp: -1 }) // Sort by timestamp in descending order (most recent first)
+    .limit(1) // Limit the result to just one document
+    .toArray();
+  console.log(latestPost);
+
+  return { props: { isPreview: false, latestPost } };
 };
 
 export default function Home(props) {
   // Scroll to top on mount as to ensure the user sees the "Preview Mode" bar
   useScrollReset();
+  const { latestPost } = props;
+  console.log(latestPost);
+
+  const [latestPostContents, setLatestPostContents] = useState([]);
+  useEffect(() => {
+    if (latestPost) {
+      setLatestPostContents(latestPost[0].contents);
+    }
+  }, [latestPost]);
 
   const [currentSnapshotId, setSnapshotId] = useState(null);
   const clearSnapshot = useCallback(() => setSnapshotId(null), [setSnapshotId]);
@@ -93,16 +106,16 @@ export default function Home(props) {
     if (hasSaveRequest.current) return;
     setSharing(true);
 
-    const els = document.querySelectorAll('[id] > [contenteditable=true]');
+    const els = document.querySelectorAll("[id] > [contenteditable=true]");
     const persistContents: FieldEdit[] = [].slice
       .call(els)
       .map(({ parentNode: { id }, innerText }) => ({ id, innerText }));
 
     self
       .fetch(`/api/save`, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(persistContents),
-        headers: { 'content-type': 'application/json' },
+        headers: { "content-type": "application/json" },
       })
       .then((res) => {
         if (res.ok) return res.json();
@@ -122,6 +135,7 @@ export default function Home(props) {
   }, []);
 
   const edits = props.isPreview ? props.contents : [];
+
   return (
     <>
       <Head>
@@ -156,7 +170,7 @@ export default function Home(props) {
           <>
             <h1>Oops</h1>
             <h2>Something unique to your preview went wrong.</h2>
-            <div className="explanation" style={{ textAlign: 'center' }}>
+            <div className="explanation" style={{ textAlign: "center" }}>
               <p>
                 The production website is <strong>still available</strong> and
                 this does not affect other users.
@@ -164,12 +178,16 @@ export default function Home(props) {
             </div>
             <hr />
             <h2>Reason</h2>
-            <div className="explanation" style={{ textAlign: 'center' }}>
+            <div className="explanation" style={{ textAlign: "center" }}>
               <p>{props.message}</p>
             </div>
           </>
         ) : (
-          <Content isEdit={isEdit} edits={edits} />
+          <Content
+            isEdit={isEdit}
+            edits={edits}
+            latestPostContents={latestPostContents}
+          />
         )}
       </div>
       {isEdit ? (
@@ -187,11 +205,29 @@ export default function Home(props) {
   );
 }
 
-function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
+function Content({
+  isEdit,
+  edits,
+  latestPostContents,
+}: {
+  isEdit: boolean;
+  edits: FieldEdit[];
+  latestPostContents: FieldEdit[];
+}) {
+  console.log(latestPostContents);
+  const idToInnerTextMap = useMemo(
+    () =>
+      latestPostContents.reduce((acc, item) => {
+        acc[item.id] = item.innerText;
+        return acc;
+      }, {}),
+    [latestPostContents]
+  );
+
   return (
     <>
       <Malleable id="title" as="h1" isActive={isEdit} edits={edits}>
-        My Static Site
+        {idToInnerTextMap["title"]}
       </Malleable>
       <div className="features">
         <div className="feature">
@@ -201,7 +237,7 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            ⚡
+            {idToInnerTextMap["feature-1-emoji"]}
           </Malleable>
           <Malleable
             id="feature-1-text"
@@ -209,7 +245,7 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            Blazing fast
+            {idToInnerTextMap["feature-1-text"]}
           </Malleable>
         </div>
         <div className="feature">
@@ -219,7 +255,7 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            📡
+            {idToInnerTextMap["feature-2-emoji"]}
           </Malleable>
           <Malleable
             id="feature-2-text"
@@ -227,7 +263,7 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            Always available
+            {idToInnerTextMap["feature-2-text"]}
           </Malleable>
         </div>
         <div className="feature">
@@ -237,7 +273,7 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            🏎
+            {idToInnerTextMap["feature-3-emoji"]}
           </Malleable>
           <Malleable
             id="feature-3-text"
@@ -245,32 +281,12 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            Lighthouse 100
+            {idToInnerTextMap["feature-3-text"]}
           </Malleable>
         </div>
       </div>
       <Malleable as="h2" id="title-2" isActive={isEdit} edits={edits}>
-        This demonstrates a static website generated using{' '}
-        <a target="_blank" rel="noopener" href="https://nextjs.org">
-          Next.js'
-        </a>{' '}
-        new{' '}
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://nextjs.org/docs/basic-features/data-fetching/get-static-props"
-        >
-          Static Site Generation (SSG)
-        </a>
-        . View the source on{' '}
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://github.com/vercel/preview-mode-demo"
-        >
-          GitHub
-        </a>
-        .
+        {idToInnerTextMap["title-2"]}
       </Malleable>
       <div className="explanation">
         <div className="p">
@@ -280,15 +296,7 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            To inspect the response from{' '}
-            <a
-              target="_blank"
-              rel="noopener"
-              href="https://vercel.com/docs/concepts/edge-network/overview"
-            >
-              our edge network
-            </a>
-            , run:
+            {idToInnerTextMap["explanation-1-inspect"]}
           </Malleable>
           <br />
           <Malleable
@@ -297,7 +305,7 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            curl -sI https://next-preview.vercel.app | grep x-vercel
+            {idToInnerTextMap["explanation-1-pre-curl"]}
           </Malleable>
           <Malleable
             id="explanation-1-pre-response"
@@ -306,41 +314,17 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             isActive={isEdit}
             edits={edits}
           >
-            x-vercel-cache: HIT
-            <br />
-            x-vercel-id: cle1::zzq7g-1604525989923-a33b3946ccee
+            {idToInnerTextMap["explanation-1-pre-response"]}
           </Malleable>
         </div>
         <Malleable id="explanation-2" isActive={isEdit} edits={edits}>
-          When people visit this site, the response always comes instantly from
-          their{' '}
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://vercel.com/docs/concepts/edge-network/overview"
-          >
-            nearest location
-          </a>
-          .
+          {idToInnerTextMap["explanation-2"]}
         </Malleable>
         <Malleable id="explanation-3" isActive={isEdit} edits={edits}>
-          Unlike traditional static solutions, however, you can generate
-          previews of edits that you can share with anyone you choose. To try it
-          out, click the edit icon on the bottom right and edit the content.
-          When you're done, click the share icon on the bottom right to generate
-          a shareable preview URL.
+          {idToInnerTextMap["explanation-3"]}
         </Malleable>
         <Malleable id="explanation-4" isActive={isEdit} edits={edits}>
-          SSG and Preview Mode make Next.js the most optimal framework to
-          integrate into your Headless CMS workflow. Learn more about the
-          preview mode on{' '}
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://nextjs.org/docs/advanced-features/preview-mode"
-          >
-            our documentation.
-          </a>
+          {idToInnerTextMap["explanation-4"]}
         </Malleable>
       </div>
     </>
